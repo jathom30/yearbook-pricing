0fc74f55c4271c6180cdf218757250431bb3cd94
Jeff Thomas <jathom30@gmail.com>
2018-02-21 11:46:39 -0500

    final svg icon id attr update

--- src/assets/PanelIcon.js
+++ src/assets/PanelIcon.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const PanelIcon = (props) => {
   return(
-    <svg alt={`${props.data.title} Icon`} id="main-icon" xmlns="http://www.w3.org/2000/svg" width="720" height="720" viewBox="0 0 720 720">
+    <svg alt={`${props.data.title} Icon`} className="main-icon" xmlns="http://www.w3.org/2000/svg" width="720" height="720" viewBox="0 0 720 720">
       <circle className="icon-circle" fill="#478BAA" cx="360" cy="357.003" r="331.5"/>
       <path className="icon-shadow" fill="#23718C" d={props.data.shadow} />
       <path className="icons" fill="#8ABFCD" d={props.data.icons} />
