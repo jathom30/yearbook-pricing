a0b515752456770a6c9d1cd4591fe7fe449dc847
Jeff Thomas <jathom30@gmail.com>
2018-02-21 11:38:25 -0500

    updated id on svg's to className so no dups. Also added alt attrs for screen readers

--- src/assets/PanelIcon.js
+++ src/assets/PanelIcon.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const PanelIcon = (props) => {
   return(
-    <svg id="main-icon" xmlns="http://www.w3.org/2000/svg" width="720" height="720" viewBox="0 0 720 720">
-      <circle id="icon-circle" fill="#478BAA" cx="360" cy="357.003" r="331.5"/>
-      <path id="icon-shadow" fill="#23718C" d={props.data.shadow} />
-      <path id="icons" fill="#8ABFCD" d={props.data.icons} />
+    <svg alt={`${props.data.title} Icon`} id="main-icon" xmlns="http://www.w3.org/2000/svg" width="720" height="720" viewBox="0 0 720 720">
+      <circle className="icon-circle" fill="#478BAA" cx="360" cy="357.003" r="331.5"/>
+      <path className="icon-shadow" fill="#23718C" d={props.data.shadow} />
+      <path className="icons" fill="#8ABFCD" d={props.data.icons} />
     </svg>
   );
 }
