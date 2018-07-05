2b3bf37c98e0a6d8de5bd11857efc78dd6c489ba
Jeff Thomas <jathom30@gmail.com>
2018-07-05 14:19:58 -0400

    added suggested due dates

--- src/components/school/StepTwo.js
+++ src/components/school/StepTwo.js
@@ -16,8 +16,15 @@ export default class StepTwo extends Component {
             <p>Staff List</p>
             <p>Team/Group photos</p>
             <p><a href="https://www.dropbox.com/sh/tjwz3xcryhzh39h/AAAMwS_AzFmYYyaC6V7aTuN7a?dl=0" target="_blank" rel="noopener noreferrer">Choose Cover and Page Design</a></p>
-            <p>Due Date is up to the school, but we recommend aiming to have the books arrive at the school one month before the last day of school.</p>
-            <p>The printer needs no more than four weeks to print the books, so the yearbook should be ready to send to the printer at least <strong>two months</strong> prior to school getting out for the summer.</p>
+            <p>Due Date is primarily up to the school, but we recommend aiming to have the books arrive at the school 2-4 weeks before the last day of school.</p>
+            <p>The printer needs no more than four weeks to print the books, so the yearbook should be ready to send to the printer at least <strong>6 weeks</strong> prior to school getting out for the summer.</p>
+            <h4>Suggested due dates for 2018-2019 JCPS school year</h4>
+            <ul>
+              <li>Last day for new media: Thursday, April 11</li>
+              <li>Due to Printer: Thursday, April 18</li>
+              <li>Projected Delivery: Thursday, May 9</li>
+              <li>Last Day of School:  Thursday, May 23</li>
+            </ul>
           </div>
         </div>
       </div>
