bce5dc001ed700ce4ae106d0281f8075c0122d24
Jeff Thomas <jathom30@gmail.com>
2019-02-20 17:11:54 -0500

    fix npm vuln and contact updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,15 +3,15 @@ import React, { Component } from 'react';
 export default class Footer extends Component {
   render() {
     const { contact, contactShow } = this.props;
-    return(
+    return (
       <div className="footer">
         <h3 onClick={contact} >Contact</h3>
         <div className={`contact-info ${contactShow ? 'expand' : 'collapse'}`} >
-          <p>Jeff Thomas</p>
-          <p>email: <a href="mailto:jathom30@gmail.com?Subject=Yeabook%20Question">jathom30@gmail.com</a></p>
-          <p>cell: <a href="tel:+1-502-439-1974" rel="nofollow">502-439-1974</a></p>
+          <p>Alex Clark</p>
+          <p>email: <a href="mailto:signaturelouky@gmail.com?Subject=Yeabook%20Question">signaturelouky@gmail.com</a></p>
+          <p>cell: <a href="tel:+1-502-896-1393" rel="nofollow">502-896-1393</a></p>
         </div>
-        <p className="copyright">© copyright 2017</p>
+        <p className="copyright">© copyright 2019</p>
       </div>
     );
   }
